07aa319122baa740b847bacd17d3b5d8deb9e91f
yual-len <allenyu90@gmail.com>
2022-05-12 23:34:25 -0700

    health changes

---
+++ My project/Assets/Scripts/Health1.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Health1 : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
+
+    [Header("Components")]
+    [SerializeField] private Behaviour[] components;
+    
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+    }
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            anim.SetTrigger("hurt");
+            //iframes
+        }
+        else
+        {
+            if (!dead)
+            {
+                anim.SetTrigger("die");
+
+                foreach (Behaviour component in components)
+                    component.enabled = false;
+
+                
+                // Player
+                // if(GetComponent<PlayerMovement>() != null)
+                //     GetComponent<PlayerMovement>().enabled = false;
+                
+                // // Enemy
+                // if(GetComponent<Slime>() != null)
+                //     GetComponent<Slime>().enabled = false;
+
+                dead = true;
+            }
+            if (dead)
+            {
+                SceneManager.LoadScene("GameOverScreen");
+            }
+        }
+    }
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+}
